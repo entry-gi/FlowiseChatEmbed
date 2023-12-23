0a60dba25d242f7baf347e47cc086cd34b99d6c0
entry-gi <gi@entryer.com>
2023-12-24 07:50:01 +0900

    Remove branding 3

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -53,7 +53,7 @@ export const Badge = (props: Props) => {
       Powered by
       <a
         ref={liteBadge}
-        href={'https://flowiseai.com'}
+        // href={'https://flowiseai.com'}
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
